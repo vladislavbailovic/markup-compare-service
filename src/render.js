c2a15a6fde388918a1aa11cee64ee0242cf6fc01
vladislav <ve@incsub.com>
2019-07-20 07:19:21 +0200

    Bail out on insignificant diff percentage

--- src/render.js
+++ src/render.js
@@ -1,14 +1,16 @@
 'use strict';
 
 module.exports.html = ( name, changes ) => {
-	const style = '* { font-family: monospace; } ins { text-decoration: none; background: green; } del { text-decoration: none; background: red; }';
 	let changed = changes.difflen ? changes.difflen : changes.added + changes.removed;
 	let percentage = Math.ceil( ( changed / changes.original ) * 100 );
+	if ( 0 === percentage ) return '';
+
 	let meta = `+${changes.added} -${changes.removed} ( ${percentage}% )`;
 	let diff = changes.diff;
 	if ( percentage > 25 && diff.length > 300 ) {
 		diff = '<i>&lt;abbreviated&gt;</i>';
 	}
+	const style = '* { font-family: monospace; } ins { text-decoration: none; background: green; } del { text-decoration: none; background: red; }';
 	let html = `<div class="diff">${diff}</div><div class="meta">${meta}</div>`;
 	return `<h3>${name}</h3>${html}<style>${style}</style>`;
 };
