f632cf36255be5abad0099bd7b2d3f6043b56604
vladislav <ve@incsub.com>
2019-07-20 07:05:13 +0200

    Showing diffs only where it makes sense to do so

--- src/render.js
+++ src/render.js
@@ -3,8 +3,12 @@
 module.exports.html = ( name, changes ) => {
 	const style = '* { font-family: monospace; } ins { text-decoration: none; background: green; } del { text-decoration: none; background: red; }';
 	let changed = changes.difflen ? changes.difflen : changes.added + changes.removed;
-	let percentage = ( changed / changes.original ) * 100;
+	let percentage = Math.ceil( ( changed / changes.original ) * 100 );
 	let meta = `+${changes.added} -${changes.removed} ( ${percentage}% significant )`;
-	let html = `<div class="diff">${changes.diff}</div><div class="meta">${meta}</div>`;
+	let diff = changes.diff;
+	if ( percentage > 25 && diff.length > 300 ) {
+		diff = '<i>&lt;abbreviated&gt;</i>';
+	}
+	let html = `<div class="diff">${diff}</div><div class="meta">${meta}</div>`;
 	return `<h3>${name}</h3>${html}<style>${style}</style>`;
 };
